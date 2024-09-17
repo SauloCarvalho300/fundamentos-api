89dbe77bb0a505d677f470c7ab9143f9ecf9809e
Saulo Carvalho <saulo_carvalho123@hotmail.com>
2024-09-17 20:41:29 -0300

    feito a API para ser direcionado as portas 3333, techs e CTECH

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,21 @@
-console.log("Linekão delas");
+import { log } from 'console'
+import express from 'express'
+
+const app = express()
+
+// http:localhost:3333/
+app.get('/', (request, response) => {  
+  return response.send("Seja Bem vindo a minha API!")
+})
+
+// http:localhost:3333/techs
+app.get('/techs', (request, response) => {
+  return response.send(['HTML', 'CSS', 'JavaScript', 'Node', 'SQL'])
+})
+
+app.get('/CTECH', (request, response) => {
+  return response.send("Os melhores estão aqui")
+})
+
+// Executa a API em um servidor node na porta 3333
+app.listen(3333)
